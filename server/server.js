bce5d501aab3bfaace88ef3483884a6f9eeaded4
Nevrard <ndangaevrard@gmail.com>
2021-07-22 21:52:45 -0400

    importing schemas filesin server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+const { ApolloServer } = require('apollo-server-express');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
